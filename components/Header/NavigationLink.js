80399838e760b7864445f42ae1e3b89726dcab21
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-09-11 14:11:04 +0200

    Update nav active link color
    Co-Authored-By: Vukašin <71568418+wayz9@users.noreply.github.com>

--- components/Header/NavigationLink.js
+++ components/Header/NavigationLink.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NavigationLink = ({ url, text }) => {
+  const { asPath } = useRouter();
   return (
     <li>
       <Link href={url}>
-        <a className="hover:text-primary-500 dark:hover:text-primary-400 relative block px-3 py-2 transition">
+        <a
+          className={
+            "relative block px-3 py-2 transition dark:hover:text-primary-400 " +
+            (asPath === url ? "text-primary-500" : "hover:text-primary-500")
+          }
+        >
           {text}
         </a>
       </Link>
