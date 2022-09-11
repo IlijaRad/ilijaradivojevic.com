9f0a13ffd070a53843457f4acd301a188e5c444d
Vukašin <ventusblade1@gmail.com>
2022-09-11 13:22:06 +0200

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
+    fontSize: {
+      xs: ["0.8125rem", "1.5rem"],
+      sm: ["0.875rem", "1.5rem"],
+      base: ["1rem", "1.75rem"],
+      "4xl": ["2rem", "2.5rem"],
+      "5xl": ["3rem", "3.5rem"],
+    },
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
